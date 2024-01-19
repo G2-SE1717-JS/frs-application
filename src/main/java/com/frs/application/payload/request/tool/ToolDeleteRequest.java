6f029821fce344c7c07636c24c028d4ba2453d5f
minhpkd <pcdm9203@gmail.com>
2024-01-19 21:25:50 +0700

    Update ToolDeleteRequest.java

--- src/main/java/com/frs/application/payload/request/tool/ToolDeleteRequest.java
+++ src/main/java/com/frs/application/payload/request/tool/ToolDeleteRequest.java
@@ -1,12 +1,12 @@
-package com.frs.application.payload.request.tool;
-
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-
-public class ToolDeleteRequest {
-    private String name;
-    private String image;
-}
+//package com.frs.application.payload.request.tool;
+//
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Data
+//@NoArgsConstructor
+//
+//public class ToolDeleteRequest {
+//    private String name;
+//    private String image;
+//}
