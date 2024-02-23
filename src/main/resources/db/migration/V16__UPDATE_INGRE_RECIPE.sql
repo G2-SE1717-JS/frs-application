1d0584c6e8b0ea3d817a21d4c156a19f431a4355
Whatever0313 <105837211+nguyenhoanghuy313@users.noreply.github.com>
2024-02-23 08:40:06 +0700

    ChangeNameDB

--- src/main/resources/db/migration/V16__UPDATE_INGRE_RECIPE.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `ingre_recipe` (
-                               `ingre_id`           BIGINT       NOT NULL ,
-                               `recipe_id`          BIGINT       NOT NULL,
-                               PRIMARY KEY (`ingre_id`, `recipe_id`)
-);
