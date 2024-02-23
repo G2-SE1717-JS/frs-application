705455e5b668980b11489af0da33ee57859096b0
NguyenHuyen0410 <huyennthe161784@fpt.edu.vn>
2024-02-23 09:11:35 +0700

    delete data duplicate

--- src/main/resources/db/migration/V17__UPDATE_CATEGORY.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `category` (
-                            `id`                 BIGINT       NOT NULL AUTO_INCREMENT,
-                            `name`               VARCHAR(350) NOT NULL,
-                            `image`              LONGTEXT,
-                            `is_deleted`         BOOLEAN      NULL DEFAULT FALSE,
-                            `created_date`       DATETIME     NOT NULL,
-                            `last_modified_date` DATETIME     NOT NULL,
-                            PRIMARY KEY (`id`)
-);
